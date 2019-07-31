71ec61e084390333e134af169cfbfdabbe702fc7
Sean Ermish <SMErmish@fullsail.edu>
2019-07-31 15:26:31 -0400

    added contact, login, order components

--- p2Frontend/src/app/app.module.ts
+++ p2Frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { OrderComponent } from './components/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ContactComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
