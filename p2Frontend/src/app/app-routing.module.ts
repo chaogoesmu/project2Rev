94205634db32b6a3deea988aa4499f1d88062c62
Sean Ermish <SMErmish@fullsail.edu>
2019-08-01 10:46:33 -0400

    added routing to the home page nav bar to go to its appropriate component

--- p2Frontend/src/app/app-routing.module.ts
+++ p2Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { OrderComponent } from './components/order/order.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
 
+  {path: "login", component: LoginComponent},
+  {path: "order", component: OrderComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "register", component: RegisterComponent},
   {path: "**", component: HomeComponent}
 ];
 
