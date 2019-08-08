309a33a7ee10559ab510a85eba4fd8b6ecd3d4b3
Francis Gurahoo <f-gu@prolikewoah.com>
2019-08-08 09:43:21 -0400

    finished removing debug and unused code

--- p2Frontend/src/app/components/navbar/navbar.component.ts
+++ p2Frontend/src/app/components/navbar/navbar.component.ts
@@ -8,25 +8,10 @@ import { UserserviceService } from '../../services/userservice.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  user:User
-  job:string;
 
   constructor(private us:UserserviceService) { }
 
   ngOnInit() {
-    console.log("yes");
-    if (this.us.getUser().title === "user"){
-      this.job = "user";
-      console.log(this.job);
-    }
-    if (this.us.getUser().title === "quartermaster"){
-
-    }
-    else{
-      this.job ="";
-    }
-
-
   }
 
 }
--- p2Frontend/src/app/services/userservice.service.ts
+++ p2Frontend/src/app/services/userservice.service.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class UserserviceService{
 
-  user:User;
+  public user:User;
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -20,25 +20,18 @@ export class UserserviceService{
   constructor(private http:HttpClient, private router:Router) { }
 
   loginUser(username:string, password:string){
-    this.user = new User(-1, username, password, "user");
+    this.user = new User(0, username, password, "user");
     this.http.post<User>("http://ec2-3-16-22-70.us-east-2.compute.amazonaws.com:9999/login", JSON.stringify(this.user), this.httpOptions).toPromise().then((response)=>{
       this.user = response;
-      console.log(response);
+      //console.log(response);
       
-      if(response === null){
-
-      }else{
+      if(!(response === null)){
         if (this.user.title === "user"){
           this.router.navigate(["/usercomp"]);
         }
       }
     });
     
-  }
-
-  getUser():User{
-    console.log(this.user);
-    return this.user;
   }
 
 }
