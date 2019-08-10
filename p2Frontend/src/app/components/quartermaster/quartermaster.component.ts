487875d3f71c9ccc36e008407cd4af4622253170
Sean Ermish <SMErmish@fullsail.edu>
2019-08-10 15:16:41 -0400

    quartermaster are fully functional

--- p2Frontend/src/app/components/quartermaster/quartermaster.component.ts
+++ p2Frontend/src/app/components/quartermaster/quartermaster.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserserviceService } from '../../services/userservice.service';
+import { BuildService } from '../../services/build.service';
+import { Order } from '../../models/order';
 
 @Component({
   selector: 'app-quartermaster',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuartermasterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us:UserserviceService, private bs:BuildService) { }
+  orders:Array<Order> = [];
 
   ngOnInit() {
+    this.viewBuilds();
   }
+  updateOrder(order:Order){
+    this.bs.updateBuild(order).then((response)=>{
+      this.viewBuilds();
+    });
+  }
+  viewBuilds(){
+    this.bs.getAllBuilds().then((response) =>{
+      this.orders = response;
+    });
+    }
 
 }
